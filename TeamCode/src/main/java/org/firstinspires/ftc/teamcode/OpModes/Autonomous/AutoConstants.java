27b268d22ec86a19af1889991123f2e5ff3a7a60
mukundkk <mukund.krishnakumar@gmail.com>
2021-03-08 23:06:16 -0800

    Add local autonomous code to new remote
    Transfer autonomous files that were being modified locally to new
    Git repo. Switch trajectory following to async and add pose storage
    class to hold robot pose between autonomous and teleop.
    
    Note: these autonomous opmodes are trajectory only (no mech code),
    but the comments indicate where to insert mechanism code as needed.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/AutoConstants.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.OpModes.Autonomous;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+public class AutoConstants {
+    public static Pose2d startPos = new Pose2d(-72, 48, 0);
+    public static Vector2d shootPos = new Vector2d(-6, 54);
+    public static double shootHeading = 0;
+    public static Vector2d shootPos2 = new Vector2d(-6, 48);
+    public static Vector2d stackPos = new Vector2d(-16, 48);
+    public static double stackHeading = Math.toRadians(-120);
+    public static Vector2d goalPos = new Vector2d(-36, 24);
+    public static double goalHeading = Math.toRadians(180);
+    public static Vector2d targetPos = new Vector2d(54, 60);
+    public static double targetHeading = 0;
+    public static Vector2d parkPos = new Vector2d(12, 60);
+    public static  double parkHeading = 0;
+
+}
